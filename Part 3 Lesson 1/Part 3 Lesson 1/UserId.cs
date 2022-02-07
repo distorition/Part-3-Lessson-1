116118bfc814069ccfce0e73f54bc43ceffcff88
distorition <85959781+distorition@users.noreply.github.com>
2022-02-07 22:37:43 +0300

    Add files via upload

---
+++ Part 3 Lesson 1/Part 3 Lesson 1/UserId.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3_Lesson_1
+{
+   public class UserId
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+    }
+}
